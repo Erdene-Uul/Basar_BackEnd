e30c83162ec8a7713ddbdf6d94b33459ae35fde9
З. Эрдэнэ-Уул <zerdeneuul2001@gmail.com>
2022-02-23 14:57:48 +0800

    backend

---
+++ routes/categories.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  getCategories,
+  getCategory,
+  createCategory,
+  updateCategory,
+  deleteCategory,
+} = require("../controller/categories");
+
+// api/v1/categories/:id/books
+const { getCategoryBooks } = require("../controller/books");
+router.route("/:categoryId/books").get(getCategoryBooks);
+
+//"/api/v1/categories"
+router.route("/").get(getCategories).post(createCategory);
+
+router
+  .route("/:id")
+  .get(getCategory)
+  .put(updateCategory)
+  .delete(deleteCategory);
+
+module.exports = router;
